3922a68ed6d3c5e48de8d4b64a00b30bba050cfb
Aleksandr <aleksandr.evlanicov@gmail.com>
2023-06-02 12:08:49 +0300

    Initial commit2

--- Main.java
+++ Main.java
@@ -1,4 +1,9 @@
 package GitHabUPLevelVersion;
 
+import static GitHabUPLevelVersion.MyProject.a;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(a);
+    }
 }
